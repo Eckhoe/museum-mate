5287caa586b672751652153c86564b8352cf6245
RobertMorabito <robertanthonymorabito@gmail.com>
2023-04-26 01:53:39 -0400

    Copyright notices added

--- src/ChatbotHelper.js
+++ src/ChatbotHelper.js
@@ -1,3 +1,15 @@
+//--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+// All website layouts, designs, coding and functionality are Copyright © 2023 Robert Morabito, David Bailey, Maheen Samad, Fahad Arain, Dana Dobrosavljevic, and Jordan Bharati All right reserved.
+//
+// You may not otherwise copy, modify, or distribute this website (https://museum-mate-v1.vercel.app/) or the code contained in any manner.
+// You may not remove or alter any copyright or other notice from this code or this website (https://museum-mate-v1.vercel.app/).
+// 
+// If you have further inquiry contact:
+// Robert Morabito
+// Developer
+// hello@robertmorabito.ca
+//--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
 import { GenerateBasic } from "./GPT-3";
 import { db } from './Firebase';
 import { collection, query, where, getDocs, doc } from 'firebase/firestore';
